a90728df7b2d157caeb796ec5514bf7273f70322
Shabrinabrina <akushabrina@gmail.com>
2022-03-22 20:33:29 +0700

    update homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { component } from 'react';
+import SearchBar from './feature/SearchBar';
+import ContainerSong from './feature/Song';
+
 
 function App() {
-  const client_id= process.env.REACT_APP_CLIENT_ID;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO WORLD
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="APP">
+      <SearchBar/>
+      <ContainerSong/>
+      <button class="btnsSelect">Select</button>
     </div>
   );
 }
---
+++ src/feature/Song.js
@@ -0,0 +1,14 @@
+import React, { component, useState } from 'react';
+
+const ContainerSong = () => {
+    return(
+       <div>
+       <label class="songtitle">Bohemian Rhapsody (the orignal sountrack)</label>
+       <p class="songArtist">Queen</p>
+       <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b"/>
+       </div>
+
+    )
+}
+
+export default ContainerSong;
