0ee17512b9fdf402aa9f75bba331d056b854ad90
Shabrinabrina <akushabrina@gmail.com>
2022-04-23 00:03:03 +0700

    Final Project

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -20,7 +20,7 @@ const config: IConfig = {
   SPOTIFY_BASE_URL: 'https://api.spotify.com/v1',
   SPOTIFY_SCOPE: 'playlist-modify-private',
   RESPONSE_TYPE: 'token',
-  REDIRECT_URI: 'http://localhost:3000',
+  REDIRECT_URI: getHost(),
 };
 
 export default config;
